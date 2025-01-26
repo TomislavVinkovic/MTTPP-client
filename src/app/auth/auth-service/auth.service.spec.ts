198f8133493b7e5ead899f7eddafff75456ebd43
Tomislav Vinkovic <tomislav>
2025-01-26 17:06:04 +0100

    todo list component tests

--- src/app/auth/auth-service/auth.service.spec.ts
+++ src/app/auth/auth-service/auth.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 import { provideHttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideRouter, Router } from '@angular/router';
 import { routes } from '../../app.routes';
--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,16 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AuthService } from '../auth/auth-service/auth.service';
 import { TodoService } from '../todo-service/todo.service';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { Todo } from '../models/todo';
+import { of } from 'rxjs';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
-  let todoService: jasmine.SpyObj<TodoService>;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -23,17 +26,34 @@ describe('TodoListComponent', () => {
           useValue: {}
         },
         provideAnimationsAsync(),
+        TodoService
       ]
     })
     .compileComponents();
 
-    todoService = TestBed.inject(TodoService) as jasmine.SpyObj<TodoService>;
+    todoService = TestBed.inject(TodoService);
+
+
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should have as many todos as the todos writable signal', fakeAsync(() => {
+    const mockTodos: Todo[] = [
+      { id: 'affsdfdgdfg', title: 'Test Todo 1', userId: "hjhjk", done: false },
+      { id: 'sfsdfhdsfhk', title: 'Test Todo 2', userId: "hjhjk", done: true }
+    ];
+
+    const getTodosSpy = spyOn(todoService, 'getTodos').and.returnValue(of({ todos: mockTodos, meta: { total: 0, pageSize: 10 } }));
+
+    component.getTodos();
+    fixture.detectChanges();
+    expect(component.todos().length).toBe(mockTodos.length);
+  }));
+
+  /*
+  TODO: When deleteTodo is called with a specific todo, and that todo is returned from the todo service, 
+  the todo should be removed from the todos writable signal.
+  */
 });
